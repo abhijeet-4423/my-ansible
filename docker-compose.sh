0e7b25e2b65f506df761cb62f073fc891cbdc4b6
Abhijeet <abhijeetlondhe2050@gmail.com>
2023-04-17 10:59:17 +0000

    ansible all configuration file

---
+++ docker-compose.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+
+
+sudo curl -SL https://github.com/docker/compose/releases/download/v2.17.2/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+sudo chmod -R 777 /usr/local/bin/docker-compose /usr/bin/docker-compose
